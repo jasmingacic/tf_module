87836edbc8603103858ababf8d64b795410d1556
jasmingacic <jasmin.gacic@gmail.com>
2020-05-10 02:24:00 +0200

    whatever

---
+++ module_drone/main.tf
@@ -0,0 +1,19 @@
+provider "packet" {
+  auth_token = var.api_key
+}
+
+resource "packet_device" "my_tf_server" {
+  hostname         = var.name
+  plan             = "t1.small.x86"
+  facilities       = ["ewr1"]
+  operating_system = "centos_7"
+  billing_cycle    = "hourly"
+  project_id       = var.project_id
+  provisioner "local-exec" {
+    command = "echo blah"
+  }
+}
+
+output "IP_Address" {
+  value = packet_device.my_tf_server.access_public_ipv4
+}
