fe43450684952d867ee96d44c534e612b7a829fe
Cody Hill <15899666+c0dyhi11@users.noreply.github.com>
2020-05-13 23:33:02 -0500

    Update outputs.tf

--- k3s_with_cloudflare/modules/k3s/outputs.tf
+++ k3s_with_cloudflare/modules/k3s/outputs.tf
@@ -2,9 +2,9 @@ output "public_ips" {
     value = "${packet_device.k3s_worker_nodes.*.access_public_ipv4}"
 }
 
-output "kubeconfig" {
-    value = data.template_file.kubeconfig.rendered
-}
+#output "kubeconfig" {
+#    value = data.template_file.kubeconfig.rendered
+#}
 
 output "yugabyte_pass" {
   value = random_string.yugabyte_pass.result
